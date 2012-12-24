b46a8e6f417e6607f4dc3d32a798fde9ae884a9d
Colin Snover <github.com@zetafleet.com>
2012-12-24 05:31:34 +0000

    Instrumented code proxy!

---
+++ client.js
@@ -0,0 +1,29 @@
+if (typeof process !== 'undefined' && typeof define === 'undefined') {
+	var req = require('./dojo/dojo');
+	// TODO: Fix configuration
+	req.set(__dirname + '/', undefined, [ 'dojo', { name: 'teststack', location: '.' } ]);
+	req(['teststack/client']);
+}
+else {
+	define([
+		'dojo/topic',
+		'dojo/aspect',
+		'./main',
+		'./lib/args',
+		'require'
+	], function (topic, aspect, main, args, require) {
+		aspect.after(topic, 'publish', function () {
+			console.log(arguments);
+		}, true);
+
+		if (!args.config) {
+			throw new Error('Missing "config" argument');
+		}
+
+		require([ args.config ], function (config) {
+			require(config.deps, function () {
+				main.run();
+			});
+		});
+	});
+}
--- runner.js
+++ runner.js
@@ -1,29 +1,23 @@
+/*global __dirname:false */
 if (typeof process !== 'undefined' && typeof define === 'undefined') {
 	var req = require('./dojo/dojo');
 	// TODO: Fix configuration
-	req.set(__dirname + '/', undefined, [ 'dojo', { name: 'teststack', location: '.' } ]);
-	req(['teststack/runner']);
+	req.set(__dirname + '/', undefined, [ 'dojo', 'istanbul', { name: 'teststack', location: '.' } ]);
+	req([ 'teststack/runner' ]);
 }
 else {
 	define([
-		'dojo/topic',
-		'dojo/aspect',
-		'./main',
-		'./lib/args',
-		'require'
-	], function (topic, aspect, main, args, require) {
-		aspect.after(topic, 'publish', function () {
-			console.log(arguments);
-		}, true);
-
-		if (!args.config) {
-			throw new Error('Missing "config" argument');
+		'./lib/createInstrumentationServer',
+		'dojo/node!./istanbul/lib/instrumenter',
+		'dojo/node!fs',
+		'dojo/node!path',
+		'./lib/args'
+	], function (createInstrumentationServer, Instrumenter, fs, path, args) {
+		if (!args.port) {
+			throw new Error('Required option "port" not specified');
 		}
 
-		require([ args.config ], function (config) {
-			require(config.deps, function () {
-				main.run();
-			});
-		});
+		var instrumenter = new Instrumenter({ noCompact: true, noAutoWrap: true });
+		createInstrumentationServer(args.port, instrumenter, '.');
 	});
 }
