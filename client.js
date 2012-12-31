2afcb2e9fd4ee12afe5fd6e6e8990503562b181a
Colin Snover <github.com@zetafleet.com>
2012-12-31 05:02:40 +0000

    If an unhandled exception occurs on the client, make sure that it is reported so the runner can do something about it.
    A test failure should not count as an error.
    
    Also updates to a new version of Dojo with a fixed loader for IE9.

--- client.js
+++ client.js
@@ -13,8 +13,9 @@ else {
 	define([
 		'./main',
 		'./lib/args',
+		'dojo-ts/topic',
 		'require'
-	], function (main, args, require) {
+	], function (main, args, topic, require) {
 		if (!args.suites) {
 			throw new Error('Missing "suites" argument');
 		}
@@ -30,6 +31,20 @@ else {
 		if (!args.reporter) {
 			console.info('Defaulting to "console" reporter');
 			args.reporter = 'console';
+		}
+
+		// TODO: This is probably a fatal condition and so we need to let the runner know that no more information
+		// will be forthcoming from this browser
+		if (typeof window !== 'undefined') {
+			window.onerror = function (message, url, lineNumber) {
+				var error = new Error(message + ' at ' + url + ':' + lineNumber);
+				topic.publish('/error', error);
+			};
+		}
+		else if (typeof process !== 'undefined') {
+			process.on('uncaughtException', function (error) {
+				topic.publish('/error', error);
+			});
 		}
 
 		// Allow 3rd party reporters to be used simply by specifying a full mid, or built-in reporters by
