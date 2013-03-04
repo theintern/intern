53fe97a65d9047efbb0d69f30072a077629bbc00
Colin Snover <github.com@zetafleet.com>
2013-03-04 20:28:11 +0000

    Make configuration of client easier by enabling a config option. Fixes #16.
    Conflicts:
    	client.js

--- client.js
+++ client.js
@@ -26,49 +26,58 @@ else {
 		'dojo-ts/topic',
 		'require'
 	], function (main, args, Suite, JSON, topic, require) {
-		if (!args.suites) {
-			throw new Error('Missing "suites" argument');
+		if (!args.config) {
+			throw new Error('Missing "config" argument');
 		}
 
-		if (args.packages) {
+		require([ args.config ], function (config) {
 			// TODO: Use of the global require is required for this to work because config mechanics are in global
 			// require only in the Dojo loader; this should probably not be the case
-			this.require({ packages: JSON.parse(args.packages) });
-		}
+			this.require(config.loader);
 
-		var deps = [].concat(args.suites);
-
-		if (!args.reporter) {
-			console.info('Defaulting to "console" reporter');
-			args.reporter = 'console';
-		}
-
-		// TODO: This is probably a fatal condition and so we need to let the runner know that no more information
-		// will be forthcoming from this client
-		if (typeof window !== 'undefined') {
-			window.onerror = function (message, url, lineNumber) {
-				var error = new Error(message + ' at ' + url + ':' + lineNumber);
-				topic.publish('/error', error);
-			};
-		}
-		else if (typeof process !== 'undefined') {
-			process.on('uncaughtException', function (error) {
-				topic.publish('/error', error);
-			});
-		}
-
-		// Allow 3rd party reporters to be used simply by specifying a full mid, or built-in reporters by
-		// specifying the reporter name only
-		deps.push(args.reporter.indexOf('/') > -1 ? args.reporter : './lib/reporters/' + args.reporter);
-
-		// Client interface has only one environment, the current environment, and cannot run functional tests on
-		// itself
-		main.suites.push(new Suite({ name: 'main', sessionId: args.sessionId }));
-
-		require(deps, function () {
-			if (args.autoRun !== 'false') {
-				main.run();
+			if (!args.suites) {
+				args.suites = config.suites;
 			}
+
+			var deps = args.suites instanceof Array ? args.suites.slice(0) : [ args.suites ];
+
+			if (!args.reporter) {
+				if (config.reporter) {
+					args.reporter = config.reporter;
+				}
+				else {
+					console.info('Defaulting to "console" reporter');
+					args.reporter = 'console';
+				}
+			}
+
+			// TODO: This is probably a fatal condition and so we need to let the runner know that no more information
+			// will be forthcoming from this client
+			if (typeof window !== 'undefined') {
+				window.onerror = function (message, url, lineNumber) {
+					var error = new Error(message + ' at ' + url + ':' + lineNumber);
+					topic.publish('/error', error);
+				};
+			}
+			else if (typeof process !== 'undefined') {
+				process.on('uncaughtException', function (error) {
+					topic.publish('/error', error);
+				});
+			}
+
+			// Allow 3rd party reporters to be used simply by specifying a full mid, or built-in reporters by
+			// specifying the reporter name only
+			deps.push(args.reporter.indexOf('/') > -1 ? args.reporter : './lib/reporters/' + args.reporter);
+
+			// Client interface has only one environment, the current environment, and cannot run functional tests on
+			// itself
+			main.suites.push(new Suite({ name: 'main', sessionId: args.sessionId }));
+
+			require(deps, function () {
+				if (args.autoRun !== 'false') {
+					main.run();
+				}
+			});
 		});
 	});
 }
