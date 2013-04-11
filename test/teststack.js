91064251f6ffa738fb7e3f682ec936b985a4c17c
Colin Snover <github.com@zetafleet.com>
2013-04-11 17:07:28 +0000

    Drop Safari 5.1 support, at least temporarily.

--- test/teststack.js
+++ test/teststack.js
@@ -23,8 +23,7 @@ define({
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 2008' },
 		{ browserName: 'firefox', version: '19', platform: [ 'Linux', 'Mac 10.6', 'Windows 2012' ] },
 		{ browserName: 'chrome', platform: [ 'Linux', 'Mac 10.8', 'Windows 2008' ] },
-		{ browserName: 'safari', version: '6', platform: 'Mac 10.8' },
-		{ browserName: 'safari', version: '5', platform: 'Mac 10.6' }
+		{ browserName: 'safari', version: '6', platform: 'Mac 10.8' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
