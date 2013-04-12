4b3410b196656f4dcb4f7092a9ff7b54507e23fa
Colin Snover <github.com@zetafleet.com>
2013-04-12 02:59:31 +0000

    Upgrade Firefox tests to version 20.

--- test/teststack.js
+++ test/teststack.js
@@ -21,7 +21,7 @@ define({
 	environments: [
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 2012' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 2008' },
-		{ browserName: 'firefox', version: '19', platform: [ 'Linux', 'Mac 10.6', 'Windows 2012' ] },
+		{ browserName: 'firefox', version: '20', platform: [ 'Linux', 'Mac 10.6', 'Windows 2012' ] },
 		{ browserName: 'chrome', platform: [ 'Linux', 'Mac 10.8', 'Windows 2008' ] },
 		{ browserName: 'safari', version: '6', platform: 'Mac 10.8' }
 	],
