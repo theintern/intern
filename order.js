3693fe85a268154612798bb41e099278ceffa0e5
Paul Bouchon <paulbouchon90@gmail.com>
2013-05-20 19:25:19 +0000

    Add plugin for loading non-AMD dependencies. Fixes #30.

---
+++ order.js
@@ -0,0 +1,28 @@
+define([
+	'./lib/util',
+	'dojo/Deferred'
+], function (util, Deferred) {
+	var queue = util.createQueue(1);
+
+	return {
+		/**
+		 * AMD plugin for in-order loading of non-AMD JavaScript. Use this when you need to test modules that do not
+		 * have proper dependency management.
+		 *
+		 * @example
+		 * define([ 'intern!object', 'intern/order!jquery.js', 'intern/order!plugin.jquery.js', ... ], ...)
+		 */
+		load: function (id, parentRequire, callback) {
+			queue(function () {
+				var dfd = new Deferred();
+
+				parentRequire([ id ], function (value) {
+					callback(value);
+					dfd.resolve();
+				});
+
+				return dfd;
+			})();
+		}
+	};
+});
