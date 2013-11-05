5269bc5e8db71e814d355eedc4433ebebcb1cb15
Colin Snover <github.com@zetafleet.com>
2013-11-05 20:10:56 +0000

    Update environments for self-test

--- tests/selftest.intern.js
+++ tests/selftest.intern.js
@@ -12,7 +12,7 @@ define({
 	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
 	// automatically
 	capabilities: {
-		'selenium-version': '2.35.0',
+		'selenium-version': '2.37.0',
 		'idle-timeout': 30
 	},
 
@@ -20,12 +20,13 @@ define({
 	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 	// capabilities options specified for an environment will be copied as-is
 	environments: [
+		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '23', platform: [ 'Linux', 'Windows 7' ] },
-		{ browserName: 'firefox', version: '21', platform: 'Mac 10.6' },
-		{ browserName: 'chrome', platform: [ 'Linux', 'Mac 10.8', 'Windows 7' ] },
-		{ browserName: 'safari', version: '6', platform: 'Mac 10.8' }
+		{ browserName: 'firefox', version: '25', platform: [ 'OS X 10.6', 'Windows 7' ] },
+		{ browserName: 'firefox', version: '24', platform: 'Linux' },
+		{ browserName: 'chrome', version: '', platform: [ 'Linux', 'OS X 10.6', 'Windows 7' ] },
+		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
