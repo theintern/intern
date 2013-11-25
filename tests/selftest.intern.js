efb39b326b6ca784ff0117ab33f76d940c7336eb
Colin Snover <github.com@zetafleet.com>
2013-11-25 06:18:01 +0000

    Update browser versions
    Seems empty-version Chrome got stuck at 28, which is not very
    helpful.

--- tests/selftest.intern.js
+++ tests/selftest.intern.js
@@ -23,9 +23,10 @@ define({
 		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '25', platform: [ 'OS X 10.6', 'Windows 7' ] },
-		{ browserName: 'firefox', version: '24', platform: 'Linux' },
-		{ browserName: 'chrome', version: '', platform: [ 'Linux', 'OS X 10.6', 'Windows 7' ] },
+		{ browserName: 'firefox', version: '25', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
+		{ browserName: 'chrome', version: '31', platform: 'Windows 7' },
+		{ browserName: 'chrome', version: '30', platform: 'Linux' },
+		{ browserName: 'chrome', version: '27', platform: 'OS X 10.8' },
 		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }
 	],
 
