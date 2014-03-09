dd74892048bb872d5f6cb487b6b864041247b6a6
Colin Snover <github.com@zetafleet.com>
2014-03-09 18:59:33 +0000

    Update Selenium and browser versions

--- tests/example.intern.js
+++ tests/example.intern.js
@@ -15,7 +15,7 @@ define({
 	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
 	// automatically
 	capabilities: {
-		'selenium-version': '2.37.0'
+		'selenium-version': '2.39.0'
 	},
 
 	// Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
@@ -25,10 +25,10 @@ define({
 		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '25', platform: [ 'OS X 10.6', 'Windows 7' ] },
-		{ browserName: 'firefox', version: '24', platform: 'Linux' },
-		{ browserName: 'chrome', version: '', platform: [ 'Linux', 'OS X 10.6', 'Windows 7' ] },
-		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }
+		{ browserName: 'firefox', version: '27', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
+		{ browserName: 'chrome', version: '32', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
+		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
+		{ browserName: 'safari', version: '7', platform: 'OS X 10.9' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
--- tests/selftest.intern.js
+++ tests/selftest.intern.js
@@ -12,7 +12,7 @@ define({
 	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
 	// automatically
 	capabilities: {
-		'selenium-version': '2.37.0',
+		'selenium-version': '2.39.0',
 		'idle-timeout': 30
 	},
 
@@ -23,11 +23,10 @@ define({
 		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '25', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
-		{ browserName: 'chrome', version: '31', platform: 'Windows 7' },
-		{ browserName: 'chrome', version: '30', platform: 'Linux' },
-		{ browserName: 'chrome', version: '27', platform: 'OS X 10.8' },
-		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }
+		{ browserName: 'firefox', version: '27', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
+		{ browserName: 'chrome', version: '32', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
+		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
+		{ browserName: 'safari', version: '7', platform: 'OS X 10.9' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
