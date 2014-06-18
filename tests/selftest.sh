16fefb41720bec7e074dce8e13045e1ce6e8739b
Colin Snover <github.com@zetafleet.com>
2014-06-17 21:25:28 -0400

    Add combined output reporter for Node.js + runner testing

--- tests/selftest.sh
+++ tests/selftest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname $0)/.."
-node_modules/.bin/intern-client config=tests/selftest.intern selftest=true
-node_modules/.bin/intern-runner config=tests/selftest.intern selftest=true
+node_modules/.bin/intern-client config=tests/selftest.intern reporters=intern-selftest/tests/combined-reporter selftest=true $@
+node_modules/.bin/intern-runner config=tests/selftest.intern reporters=intern-selftest/tests/combined-reporter selftest=true $@
