7ea89d47b234ba4fda94adddd6ec163d62a67e9b
Colin Snover <github.com@zetafleet.com>
2014-06-18 08:59:30 -0400

    Update default browser versions

--- tests/selftest.intern.js
+++ tests/selftest.intern.js
@@ -12,7 +12,7 @@ define({
 	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
 	// automatically
 	capabilities: {
-		'selenium-version': '2.39.0',
+		'selenium-version': '2.41.0',
 		'idle-timeout': 30
 	},
 
@@ -23,9 +23,8 @@ define({
 		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '27', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
-		{ browserName: 'chrome', version: '32', platform: [ 'OS X 10.6', 'Windows 7', 'Linux' ] },
-		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
+		{ browserName: 'firefox', version: '28', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
+		{ browserName: 'chrome', version: '34', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
 		{ browserName: 'safari', version: '7', platform: 'OS X 10.9' }
 	],
 
