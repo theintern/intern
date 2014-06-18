e9360701376f7e7915d1459d218cfd59fce8c040
Colin Snover <github.com@zetafleet.com>
2014-06-18 09:04:50 -0400

    Ensure AMD dependencies are always in the expected place
    npm will deduplicate dependencies by omitting them from children
    and expecting that filesystem walking will occur. The loader could
    be modified to perform this action, but when client.html is loaded
    directly by an end-user, it would not be possible to do this. As
    a result, we simply make sure that symlinks to dependencies loaded
    via AMD on the client are always put in place. (Dependencies loaded
    as Node.js modules already work properly.)

---
+++ .fixdeps.js
@@ -0,0 +1,29 @@
+/*jshint node:true */
+var fs = require('fs');
+var path = require('path');
+var expected = path.join(__dirname, 'node_modules');
+
+// AMD-loaded dependencies need to exist in Intern's node_modules directory,
+// regardless of whether or not they were deduped by npm
+[ 'dojo', 'chai' ].forEach(function (dependency) {
+	var expectedPath = path.join(expected, dependency);
+
+	// Reset any existing dependency symlinks in case the location of the
+	// deduplicated package has changed
+	try {
+		if (fs.lstatSync(expectedPath).isSymbolicLink()) {
+			fs.unlinkSync(expectedPath);
+		}
+	}
+	catch (error) {
+		if (error.code !== 'ENOENT') {
+			throw error;
+		}
+	}
+
+	var actualPath = path.dirname(require.resolve(dependency));
+
+	if (actualPath.indexOf(expectedPath) !== 0) {
+		fs.symlinkSync(actualPath, expectedPath, 'dir');
+	}
+});
