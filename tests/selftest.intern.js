a0ff28e7f1a03969604f5190685127d34599a775
Jason Cheatham <j.cheatham@gmail.com>
2014-06-19 14:52:28 -0400

    Publish Leadfoot deprecation messages in Intern
    Leadfoot and Intern use different dojo's, so messages have to be pulled
    from one to the other.

--- tests/selftest.intern.js
+++ tests/selftest.intern.js
@@ -48,7 +48,10 @@ define({
 	],
 
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
-	functionalSuites: [ 'intern-selftest/tests/functional/lib/ProxiedSession' ],
+	functionalSuites: [
+		'intern-selftest/tests/functional/lib/ProxiedSession',
+		'intern-selftest/tests/functional/compat'
+	],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
 	excludeInstrumentation: /^(?:tests|node_modules)\//,
