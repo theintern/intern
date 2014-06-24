0a1b140da680e15ac4156a7b4b703c1ced585ea4
Jason Cheatham <j.cheatham@gmail.com>
2014-06-24 09:29:57 -0400

    Fix compat deprecation warning test
    Last update broke it

--- tests/functional/compat.js
+++ tests/functional/compat.js
@@ -3,12 +3,18 @@ define([
 	'intern!object',
 	'intern/chai!assert',
 	'dojo/node!leadfoot/compat',
+	'dojo/topic',
 	// require the runner reporter to hook up a /deprecated topic listener
 	'../../lib/reporters/runner'
-], function (registerSuite, assert, compat) {
-	var deprecationMessage;
+], function (registerSuite, assert, compat, topic) {
+	var deprecatedMethod;
 	var command = { setFindTimeout: function () {} };
 	compat.applyTo(command);
+
+	var handle = topic.subscribe('/deprecated', function (name) {
+		deprecatedMethod = name;
+		handle.remove();
+	});
 
 	registerSuite({
 		name: 'compat',
@@ -16,17 +22,11 @@ define([
 		// Verify that leadfoot/compat deprecation messages are received by the runner reporter. This test is only
 		// meaningful the first time it's run; every other time it uses the result captured in the first run.
 		deprecation: function () {
-			if (deprecationMessage) {
-				return;
-			}
-
 			var consoleWarn = console.warn;
-			console.warn = function(message) {
-				deprecationMessage = message;
-			};
+			console.warn = function() {};
 			try {
 				command.setImplicitWaitTimeout();
-				assert.include(deprecationMessage, 'Command#setImplicitWaitTimeout is deprecated');
+				assert.equal(deprecatedMethod, 'Command#setImplicitWaitTimeout');
 			}
 			finally {
 				console.warn = consoleWarn;
