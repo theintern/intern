8a37b2c41ce1d5950e785bf3f956d37f5f40735b
Jason Cheatham <j.cheatham@gmail.com>
2014-06-25 09:10:10 -0400

    Don't use .bin paths for scripts in selftest.sh
    selftest.sh is useful during development, but when swapping back
    and forth between geezer and master the links in `node_modules/.bin`
    aren't updated. Use a direct relative path to the client and runner
    scripts instead.

--- tests/selftest.sh
+++ tests/selftest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd "$(dirname $0)/.."
-node_modules/.bin/intern-client config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined selftest=true selftest2 selftest3=a selftest3=b selftest3=c $@
-node_modules/.bin/intern-runner config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined selftest=true selftest2 selftest3=a selftest3=b selftest3=c $@
+node_modules/intern/bin/intern-client.js config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined selftest=true selftest2 selftest3=a selftest3=b selftest3=c $@
+node_modules/intern/bin/intern-runner.js config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined selftest=true selftest2 selftest3=a selftest3=b selftest3=c $@
