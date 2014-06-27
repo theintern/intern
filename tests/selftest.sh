8f715f1e04865dd751dc0b0fde9591b5dfd75277
Jason Cheatham <j.cheatham@gmail.com>
2014-06-27 13:29:11 -0400

    Add notice to selftest.sh about how to run Intern
    Point out that the script only starts Intern the way it does so that the
    script can be shared between master and geezer.
    
    [ci skip]

--- tests/selftest.sh
+++ tests/selftest.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname $0)/.."
+# We're calling intern-X.js directly here so that this script can be shared by
+# master and geezer, but intern should normally be started using the links
+# created by npm in node_modules/.bin.
 node_modules/intern/bin/intern-client.js config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined selftest=true selftest2 selftest3=a selftest3=b selftest3=c $@
 node_modules/intern/bin/intern-runner.js config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined selftest=true selftest2 selftest3=a selftest3=b selftest3=c $@
