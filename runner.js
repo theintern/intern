b9481908bed642a9994a91e4dafcb3df6660935c
Dennis Reimann <mail@dennisreimann.de>
2014-07-24 12:06:53 +0200

    Take functionalSuites from command-line arguments
    This should fix #123 and also improve the Grunt integration as it allows for specifying functionalSuites from the command-line interface, which is also used via Grunt.

--- runner.js
+++ runner.js
@@ -102,6 +102,10 @@ else {
 					args.reporters = 'runner';
 				}
 			}
+			
+			if (args.functionalSuites) {
+				config.functionalSuites = args.functionalSuites;
+			}
 
 			if (config.tunnel.indexOf('/') === -1) {
 				config.tunnel = 'dojo/node!digdug/' + config.tunnel;
