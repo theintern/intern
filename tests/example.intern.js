a3515c9e94c8080177983827a3cef2a1d77b7404
Colin Snover <github.com@zetafleet.com>
2015-04-23 03:44:49 +0000

    Update example configuration
    [ci skip]

--- tests/example.intern.js
+++ tests/example.intern.js
@@ -1,52 +1,42 @@
-// Learn more about configuring this file at <https://github.com/theintern/intern/wiki/Configuring-Intern>.
+// Learn more about configuring this file at <https://theintern.github.io/intern/#configuration>.
 // These default settings work OK for most people. The options that *must* be changed below are the
-// packages, suites, excludeInstrumentation, and (if you want functional tests) functionalSuites.
+// packages, suites, excludeInstrumentation, and (if you want functional tests) functionalSuites
 define({
-	// The port on which the instrumenting proxy will listen
-	proxyPort: 9000,
-
-	// A fully qualified URL to the Intern proxy
-	proxyUrl: 'http://localhost:9000/',
-
 	// Default desired capabilities for all environments. Individual capabilities can be overridden by any of the
 	// specified browser environments in the `environments` array below as well. See
-	// https://code.google.com/p/selenium/wiki/DesiredCapabilities for standard Selenium capabilities and
-	// https://saucelabs.com/docs/additional-config#desired-capabilities for Sauce Labs capabilities.
-	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
-	// automatically
+	// <https://theintern.github.io/intern/#option-capabilities> for links to the different capabilities options for
+	// different services.
+	//
+	// Note that the `build` capability will be filled in with the current commit ID or build tag from the CI
+	// environment automatically
 	capabilities: {
-		'selenium-version': '2.41.0'
+		'browserstack.selenium_version': '2.45.0'
 	},
 
 	// Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
 	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 	// capabilities options specified for an environment will be copied as-is
 	environments: [
-		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
-		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '28', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
-		{ browserName: 'chrome', version: '34', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
-		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
-		{ browserName: 'safari', version: '7', platform: 'OS X 10.9' }
+		{ browserName: 'internet explorer', version: '11', platform: 'WIN8' },
+		{ browserName: 'internet explorer', version: '10', platform: 'WIN8' },
+		{ browserName: 'internet explorer', version: '9', platform: 'WINDOWS' },
+		{ browserName: 'firefox', version: '37', platform: [ 'WINDOWS', 'MAC' ] },
+		{ browserName: 'chrome', version: '39', platform: [ 'WINDOWS', 'MAC' ] },
+		{ browserName: 'safari', version: '8', platform: 'MAC' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
-	maxConcurrency: 3,
+	maxConcurrency: 2,
 
-	// Name of the tunnel class to use for WebDriver tests
-	tunnel: 'SauceLabsTunnel',
+	// Name of the tunnel class to use for WebDriver tests.
+	// See <https://theintern.github.io/intern/#option-tunnel> for built-in options
+	tunnel: 'BrowserStackTunnel',
 
-	// The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
-	// loader
-	useLoader: {
-		'host-node': 'dojo/dojo',
-		'host-browser': 'node_modules/dojo/dojo.js'
-	},
-
-	// Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
-	// can be used here
-	loader: {
+	// Configuration options for the module loader; any AMD configuration options supported by the AMD loader in use
+	// can be used here.
+	// If you want to use a different loader than the default loader, see
+	// <https://theintern.github.io/intern/#option-useLoader> for instruction
+	loaderConfig: {
 		// Packages that should be registered with the loader in each testing environment
 		packages: [ { name: 'myPackage', location: '.' } ]
 	},
