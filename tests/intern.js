44b7be117d5f019368c573dc2695ce85319b2af6
Colin Snover <github.com@zetafleet.com>
2015-04-23 01:04:20 +0000

    Update Intern and Dojo dependencies

--- tests/intern.js
+++ tests/intern.js
@@ -1,32 +1,22 @@
 define({
-	proxyPort: 9000,
-	proxyUrl: 'http://localhost:9000/',
-
 	capabilities: {
-		'selenium-version': '2.41.0',
+		'selenium-version': '2.43.0',
 		'idle-timeout': 30
 	},
 
 	environments: [
-// TODO: Sauce Labs IE environments (and Android environments, and iOS environments) are B-R-O-K-E-N. Wait for Intern 2
-// and then switch providers!
-//		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
-//		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-//		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '30', platform: [ 'Windows 7', 'Linux' ] },
-		{ browserName: 'firefox', version: '29', platform: 'OS X 10.9' },
-		{ browserName: 'chrome', version: '35', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
-		{ browserName: 'safari', version: '7', platform: 'OS X 10.9' }
+		{ browserName: 'internet explorer', version: '11', platform: 'WIN8' },
+		{ browserName: 'internet explorer', version: '10', platform: 'WIN8' },
+		{ browserName: 'internet explorer', version: '9', platform: 'WINDOWS' },
+		{ browserName: 'firefox', version: '33', platform: [ 'WINDOWS', 'MAC' ] },
+		{ browserName: 'chrome', version: '38', platform: [ 'WINDOWS', 'MAC' ] },
+		{ browserName: 'safari', version: '8', platform: 'MAC' }
 	],
 
-	maxConcurrency: 3,
+	maxConcurrency: 2,
+	tunnel: 'BrowserStackTunnel',
 
-	useSauceConnect: true,
-
-	webdriver: {
-		hostname: 'localhost',
-		port: 4444
-	},
+	reporters: [ 'combined' ],
 
 	loader: {
 		packages: [
