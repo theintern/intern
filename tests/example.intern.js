c95e41dd31207d5c150f47e26a93e3f46ec5d4f8
Tony Gambone <tonygambone@gmail.com>
2015-05-21 13:19:23 -0400

    Match backslash as separator in excludeInstrumentation.
    This makes the default value function correctly on Windows.

--- tests/example.intern.js
+++ tests/example.intern.js
@@ -48,5 +48,5 @@ define({
 	functionalSuites: [ /* 'myPackage/tests/functional' */ ],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
-	excludeInstrumentation: /^(?:tests|node_modules)\//
+	excludeInstrumentation: /^(?:tests|node_modules)[\/\\]/
 });
