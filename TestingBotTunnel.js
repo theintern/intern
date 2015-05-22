3e6ed47340b2a4fb9a47f13f66fc00b76d1410e0
Colin Snover <github.com@zetafleet.com>
2015-05-22 02:43:31 +0000

    Apply fix from 8ba5dae00c3d2e57fa5d6df08e72a6f9e826f6b1 to TestingBotTunnel

--- TestingBotTunnel.js
+++ TestingBotTunnel.js
@@ -183,7 +183,12 @@ TestingBotTunnel.prototype = util.mixin(Object.create(_super), /** @lends module
 
 		// Polling API is used because we are only watching for one file, so efficiency is not a big deal, and the
 		// `fs.watch` API has extra restrictions which are best avoided
-		fs.watchFile(readyFile, { persistent: false, interval: 1007 }, function () {
+		fs.watchFile(readyFile, { persistent: false, interval: 1007 }, function (current, previous) {
+			if (Number(current.mtime) === Number(previous.mtime)) {
+				// readyFile hasn't been modified, so ignore the event
+				return;
+			}
+
 			fs.unwatchFile(readyFile);
 			dfd.resolve();
 		});
