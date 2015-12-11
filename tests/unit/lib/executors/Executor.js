d45beb65744fa74106fc07b6feb634350e873887
Colin Snover <github.com@zetafleet.com>
2015-12-10 19:09:38 -0600

    Remove blank tests

--- tests/unit/lib/executors/Executor.js
+++
@@ -1,9 +0,0 @@
-define(function (require) {
-	var assert = require('intern/chai!assert');
-	var registerSuite = require('intern!object');
-	var Executor = require('../../../../lib/executors/Executor');
-
-	registerSuite({
-		
-	});
-});
