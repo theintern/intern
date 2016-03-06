ace7aba2ff0fef1ef3fb0760790bc41d682f39d4
Jason Cheatham <j.cheatham@gmail.com>
2016-03-05 22:55:00 -0500

    apidoc generation updates
    Create stub files for doc files that have new names due to jsdocs
    namespacing rules.

--- support/apidoc.sh
+++ support/apidoc.sh
@@ -11,6 +11,39 @@ if [ "$REPLY" != "y" ]; then
 	exit 0
 fi
 
+function createRedirectFile {
+	local name
+	local new_file
+
+	name=$1
+	new_file=$2
+	title=$3
+
+	if [[ -z $title ]]; then
+		title=$name
+	fi
+
+	echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+	<meta charset=\"utf-8\">
+	<title>Leadfoot docs: Module: leadfoot/${title}</title>
+	<link rel=\"stylesheet\" href=\"styles/catalyst.css\">
+	<style>
+		p { margin: 2em; font-size: 120%; }
+	</style>
+</head>
+<body>
+	<p>This page has moved to <a id=\"redirect\" href=\"${new_file}\">${new_file}</a>.</p>
+	<script>
+		// Update the redirect URL with this page's hash fragment. This isn't guaranteed to work as anchor IDs may have changed.
+		var redirect = document.getElementById('redirect');
+		redirect.href += location.hash;
+	</script>
+</body>
+</html>" > ${name}.html
+}
+
 SUPPORT_DIR=$(cd $(dirname $0) && pwd)
 ROOT_DIR=$(cd "$SUPPORT_DIR/.." && pwd)
 BUILD_DIR="$ROOT_DIR/build_doc"
@@ -22,6 +55,14 @@ git rm -r '*'
 cd "$ROOT_DIR"
 jsdoc -c "$SUPPORT_DIR/jsdoc.conf" -t "../jsdoc-theme/catalyst/" -d "$BUILD_DIR" --verbose *.js helpers/*.js README.md
 cd "$BUILD_DIR"
+
+# Create stub files for modules that have been renamed due to jsdoc's namespacing rules
+for name in Command Element Server Session compat keys; do
+	createRedirectFile $name module-leadfoot_${name}.html
+done
+createRedirectFile pollUntil module-leadfoot_helpers_pollUntil.html "helpers/pollUntil"
+createRedirectFile pollUntil.js helpers_pollUntil.js.html "helpers/pollUntil.js"
+
 git add -A
 git commit -a -m "Rebuild documentation"
 git push origin gh-pages
