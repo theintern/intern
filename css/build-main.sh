8a426b626c5f1a789f377e8a3e51b30c8e270141
Jason Cheatham <j.cheatham@gmail.com>
2016-08-08 12:21:48 -0400

    Update for SeleniumTunnel, style updates

--- css/build-main.sh
+++ css/build-main.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-stylus --inline -c -u nib/lib/nib main.styl
+cd "$(dirname "$0")"
+stylus --inline -c -u nib -o . main.styl
 uglifyjs --screw-ie8 -m -c -- highlight.pack.js main.js > main.min.js
