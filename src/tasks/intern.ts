214c1826642f2153fd5c4034103ce695bb5aff98
Jason Cheatham <j.cheatham@gmail.com>
2017-04-11 09:05:40 -0400

    Update grunt task to work with dojo 2
    references #697
    
    [ci skip]

--- src/tasks/intern.ts
+++ src/tasks/intern.ts
@@ -1,53 +1,26 @@
-import runner from 'intern/lib/node/runner';
-import global from 'dojo-core/global';
+import runner from '../lib/node/runner';
+import { Config } from '../lib/executors/Executor';
+import global from '@dojo/core/global';
 
-interface TaskOptions extends grunt.task.ITaskOptions {
-	cwd?: string;
-	runType?: string;
+interface TaskOptions extends grunt.task.ITaskOptions, Partial<Config> {
 	[key: string]: any;
 }
 
 export = function (grunt: IGrunt) {
 	grunt.registerMultiTask('intern', function () {
 		const done = this.async();
-		const options = this.options<TaskOptions>({});
-		const skipOptions: { [key: string]: boolean } = {
-			browserstackAccessKey: true,
-			browserstackUsername: true,
-			cbtApikey: true,
-			cbtUsername: true,
-			sauceAccessKey: true,
-			sauceUsername: true,
-			testingbotKey: true,
-			testingbotSecret: true
-		};
+		const config = this.options<TaskOptions>({});
 
-		[
-			'browserstackAccessKey',
-			'browserstackUsername',
-			'cbtApikey',
-			'cbtUsername',
-			'sauceAccessKey',
-			'sauceUsername',
-			'testingbotKey',
-			'testingbotSecret'
-		].filter(option => Boolean(options[option])).forEach(option => {
-			process.env[option.replace(/[A-Z]/g, '_$&').toUpperCase()] = options[option];
-		});
-
-		// force colored output for istanbul report
+		// Force colored output for istanbul report
 		process.env.FORCE_COLOR = true;
-
-		const config: { [key: string]: any } = {};
-		Object.keys(options).filter(option => !skipOptions[option]).forEach(option => {
-			config[option] = options[option];
-		});
 
 		runner(config).then(finish, finish);
 
-		function finish() {
+		function finish(error?: any) {
+			// Remove the global intern object when we're done; this will allow Intern to be run again in the same
+			// process
 			global.intern = null;
-			done();
+			done(error);
 		}
 	});
 };
