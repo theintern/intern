ad97445edb1a0a1cd3f72e68f6b4d633567be733
Jason Cheatham <j.cheatham@gmail.com>
2017-05-01 12:50:27 -0400

    Improve WebSocket startup
    Catch errors during socket initialization and fail pending sends when an
    error occurs.
    
    [ci skip]

--- src/lib/channels/WebSocket.ts
+++ src/lib/channels/WebSocket.ts
@@ -6,7 +6,7 @@ export default class WebSocketChannel extends BaseChannel {
 	timeout: number;
 
 	protected _socket: WebSocket;
-	protected _sendQueue: { [key: string]: () => void };
+	protected _sendQueue: { [key: string]: { resolve: () => void, reject: (error: Error) => void } };
 	protected _ready: Task<any>;
 	protected _sequence: number;
 
@@ -23,12 +23,17 @@ export default class WebSocketChannel extends BaseChannel {
 
 		this._socket = new WebSocket(`ws://localhost:${options.port}`);
 
-		this._ready = new Task(resolve => {
+		this._ready = new Task((resolve, reject) => {
 			this._socket.addEventListener('open', resolve);
+			this._socket.addEventListener('error', reject);
 		});
 
 		this._socket.addEventListener('message', event => {
 			this._handleMessage(JSON.parse(event.data));
+		});
+
+		this._socket.addEventListener('error', _event => {
+			this._handleError(new Error('WebSocket error'));
 		});
 
 		this._sendQueue = {};
@@ -36,32 +41,43 @@ export default class WebSocketChannel extends BaseChannel {
 	}
 
 	protected _sendData(name: string, data: any) {
-		try {
+		return this._ready.then(() => new Task<void>((resolve, reject) => {
 			const id = String(this._sequence++);
 			const sessionId = this.sessionId;
 			const message: Message = { id, sessionId, name, data };
 
-			return this._ready.then(() => new Task<void>((resolve, reject) => {
-				this._socket.send(JSON.stringify(message));
+			this._socket.send(JSON.stringify(message));
 
-				const timer = setTimeout(() => {
-					reject(new Error('Send timed out'));
-				}, this.timeout);
+			const timer = setTimeout(() => {
+				reject(new Error('Send timed out'));
+			}, this.timeout);
 
-				this._sendQueue[id] = () => {
+			this._sendQueue[id] = {
+				resolve() {
 					clearTimeout(timer);
 					resolve();
-				};
-			}));
-		}
-		catch (error) {
-			return Task.reject<void>(error);
-		}
+				},
+				reject(error: Error) {
+					reject(error);
+				}
+			};
+		}));
 	}
 
 	protected _handleMessage(message: any) {
 		const id = message.id;
-		this._sendQueue[id]();
+		this._sendQueue[id].resolve();
 		this._sendQueue[id] = null;
+	}
+
+	protected _handleError(error: Error) {
+		// Make the _ready task a reject to reject future _sendData calls
+		this._ready = Task.reject(error);
+
+		// Reject any open sends
+		Object.keys(this._sendQueue).filter(id => this._sendQueue[id] != null).forEach(id => {
+			this._sendQueue[id].reject(error);
+			this._sendQueue[id] = null;
+		});
 	}
 }
