ea8dd1cb399eebc2b1d3a3d2dda0116afdd598da
Jason Cheatham <j.cheatham@gmail.com>
2017-06-07 07:52:03 -0400

    Add loaders/dojo unit tests
    [ci skip]

---
+++ tests/unit/loaders/dojo.ts
@@ -0,0 +1,106 @@
+import intern from '../../../src/index';
+import global from '@dojo/core/global';
+import Promise from '@dojo/shim/Promise';
+import { spy } from 'sinon';
+import { LoaderInit } from 'src/lib/executors/Executor';
+
+const { registerSuite } = intern().getPlugin('interface.object');
+const assert = intern().getPlugin('chai.assert');
+const mockRequire = intern().getPlugin<mocking.MockRequire>('mockRequire');
+
+const originalIntern = global.intern;
+const originalDojoConfig = global.dojoConfig;
+const originalRequire = global.require;
+
+registerSuite('loaders/dojo', function () {
+	let removeMocks: () => void;
+
+	const mockIntern = {
+		config: { basePath: '/' },
+		emit: spy(() => { }),
+		loadScript: spy(() => Promise.resolve()),
+		registerLoader: spy((_init: LoaderInit) => { }),
+		log: spy(() => { })
+	};
+
+	const fakeRequire: any = spy((_modules: string[], callback: () => void) => {
+		if (requirePromise) {
+			requirePromise.then(callback);
+		}
+		else {
+			callback();
+		}
+	});
+	fakeRequire.on = spy(() => {
+		return { remove() { } };
+	});
+
+	let requirePromise: Promise<void>;
+
+	return {
+		before() {
+			global.intern = mockIntern;
+			return mockRequire(require, 'src/loaders/dojo', {}).then(handle => {
+				removeMocks = handle.remove;
+				assert.equal(mockIntern.registerLoader.callCount, 1);
+			});
+		},
+
+		after() {
+			removeMocks();
+		},
+
+		beforeEach() {
+			global.intern = mockIntern;
+			global.require = fakeRequire;
+			mockIntern.emit.reset();
+			mockIntern.loadScript.reset();
+			fakeRequire.reset();
+			fakeRequire.on.reset();
+		},
+
+		afterEach() {
+			global.intern = originalIntern;
+			global.dojoConfig = originalDojoConfig;
+			global.require = originalRequire;
+		},
+
+		tests: {
+			init() {
+				const init = mockIntern.registerLoader.getCall(0).args[0];
+				return init({}).then(() => {
+					assert.equal(mockIntern.loadScript.callCount, 1);
+				});
+			},
+
+			'load Modules'() {
+				const init: LoaderInit = mockIntern.registerLoader.getCall(0).args[0];
+				return Promise.resolve(init({})).then(loader => {
+					return loader(['foo.js']).then(() => {
+						assert.equal(fakeRequire.callCount, 1);
+						assert.deepEqual(fakeRequire.getCall(0).args[0], ['foo.js']);
+					});
+				});
+			},
+
+			error() {
+				const init: LoaderInit = mockIntern.registerLoader.getCall(0).args[0];
+				return Promise.resolve(init({})).then(loader => {
+					requirePromise = new Promise<void>(() => { });
+					const error = new Error('fail');
+
+					setTimeout(() => {
+						assert.equal(fakeRequire.on.getCall(0).args[0], 'error');
+						const errorHandler = fakeRequire.on.getCall(0).args[1];
+						errorHandler(error);
+					});
+
+					return loader(['foo.js']).then(
+						() => { throw new Error('should not have succeeded'); },
+						error => { assert.match(error.message, /Dojo loader error/); }
+					);
+				});
+			}
+		}
+	};
+});
