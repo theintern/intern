0929ff97496e965cca04426949b2ecd885ec3c13
Jason Cheatham <j.cheatham@gmail.com>
2017-06-07 01:24:37 -0400

    Improve the node mocking plugin
    - Ensure original module is deleted from the cache
    - Provide a way to simply delete a loaded module without providing a
      mock, so that it will be reloaded when next requested
    
    [ci skip]

--- tests/nodeMocking.ts
+++ tests/nodeMocking.ts
@@ -1,27 +1,37 @@
 intern.registerPlugin('mockRequire', () => {
 	function mockRequire(require: NodeRequire, mod: string, mocks: { [name: string]: any }) {
-		const registeredMocks: { id: string, original: any, require: NodeRequire }[] = [];
+		const registeredMocks: { id: string, original: any }[] = [];
 
 		mod = require.resolve(mod);
-		registeredMocks.push({ id: mod, original: undefined, require });
+		registeredMocks.push({ id: mod, original: require.cache[mod] });
+		delete require.cache[mod];
+
 		Object.keys(mocks).forEach(name => {
 			const id = require.resolve(name);
-			registeredMocks.push({ id, original: require.cache[id], require });
-			require.cache[id] = {
-				id,
-				filename: id,
-				loaded: true,
-				exports: mocks[name]
-			};
+			registeredMocks.push({ id, original: require.cache[id] });
+			delete require.cache[id];
+
+			// If mocks[name] is null then there's no explicit mock for the module, it just needs to be reloaded to use
+			// mocks that have just been defined.
+			if (mocks[name] != null) {
+				require.cache[id] = {
+					id,
+					filename: id,
+					loaded: true,
+					exports: mocks[name]
+				};
+			}
 		});
-		delete require.cache[mod];
 
 		return Promise.resolve({
 			module: require(mod),
 			remove() {
 				while (registeredMocks.length > 0) {
-					const { id, original, require } = registeredMocks.pop()!;
-					require.cache[id] = original;
+					const { id, original } = registeredMocks.pop()!;
+					delete require.cache[id];
+					if (typeof original !== 'undefined') {
+						require.cache[id] = original;
+					}
 				}
 			}
 		});
