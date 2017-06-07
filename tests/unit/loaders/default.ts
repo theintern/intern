fe5f9492d437e8f73866c41c8b0393aaf398ebda
Jason Cheatham <j.cheatham@gmail.com>
2017-06-07 09:11:37 -0400

    Add loaders/default unit tests
    [ci skip]

---
+++ tests/unit/loaders/default.ts
@@ -0,0 +1,81 @@
+import intern from '../../../src/index';
+import global from '@dojo/core/global';
+import Promise from '@dojo/shim/Promise';
+import { spy, stub } from 'sinon';
+import { LoaderInit } from 'src/lib/executors/Executor';
+
+const { registerSuite } = intern().getPlugin('interface.object');
+const assert = intern().getPlugin('chai.assert');
+const mockRequire = intern().getPlugin<mocking.MockRequire>('mockRequire');
+
+const originalIntern = global.intern;
+const originalRequire = global.require;
+
+registerSuite('loaders/default', function () {
+	let removeMocks: () => void;
+
+	const mockIntern = {
+		config: { basePath: '/' },
+		emit: spy(() => { }),
+		loadScript: stub().resolves(),
+		registerLoader: spy((_init: LoaderInit) => { }),
+		log: spy(() => { })
+	};
+
+	return {
+		before() {
+			global.intern = mockIntern;
+			return mockRequire(require, 'src/loaders/default', {}).then(handle => {
+				removeMocks = handle.remove;
+				assert.equal(mockIntern.registerLoader.callCount, 1);
+			});
+		},
+
+		after() {
+			removeMocks();
+		},
+
+		beforeEach() {
+			global.intern = mockIntern;
+			mockIntern.emit.reset();
+			mockIntern.loadScript.reset();
+			mockIntern.loadScript.resolves();
+		},
+
+		afterEach() {
+			global.intern = originalIntern;
+			global.require = originalRequire;
+		},
+
+		tests: {
+			init() {
+				const init = mockIntern.registerLoader.getCall(0).args[0];
+				return Promise.resolve(init({})).then(() => {
+					// The default loader doesn't do anythign in its init function
+					assert.equal(mockIntern.loadScript.callCount, 0);
+				});
+			},
+
+			'load Modules'() {
+				const init: LoaderInit = mockIntern.registerLoader.getCall(0).args[0];
+				return Promise.resolve(init({})).then(loader => {
+					return loader(['foo.js']).then(() => {
+						assert.equal(mockIntern.loadScript.callCount, 1);
+						assert.equal(mockIntern.loadScript.getCall(0).args[0], 'foo.js');
+					});
+				});
+			},
+
+			error() {
+				const init: LoaderInit = mockIntern.registerLoader.getCall(0).args[0];
+				mockIntern.loadScript.rejects(new Error('fail'));
+				return Promise.resolve(init({})).then(loader => {
+					return loader(['foo.js']).then(
+						() => { throw new Error('should not have succeeded'); },
+						error => { assert.match(error.message, /fail/); }
+					);
+				});
+			}
+		}
+	};
+});
