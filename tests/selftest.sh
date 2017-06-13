787386c8036b0d594c4a0fac1ff932168fbd6dfc
Jason Cheatham <j.cheatham@gmail.com>
2017-06-13 11:21:48 -0400

    Self-test cleanup
    Remove unused files, reorganize tests directory.

--- tests/selftest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname $0)/.."
-npm run build
-node_modules/.bin/intern-client config=_build/tests/selftest.intern reporters=Combined selftest=true selftest2 selftest3=a selftest3=b selftest3=c $@
-if [[ $INTERN_NODE_ONLY != "1" ]]; then
-	node_modules/.bin/intern-runner config=_build/tests/selftest.intern reporters=Combined selftest=true selftest2 selftest3=a selftest3=b selftest3=c $@
-fi
