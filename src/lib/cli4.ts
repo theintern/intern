0bd9b56303e64ca1e9cd68e32a8ae2689ca326eb
Jason Cheatham <j.cheatham@gmail.com>
2017-09-08 16:35:51 -0400

    Add basic watcher support for Intern 4

--- src/lib/cli4.ts
+++ src/lib/cli4.ts
@@ -3,7 +3,8 @@ import * as opn from 'opn';
 import { existsSync, mkdirSync, statSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { createInterface } from 'readline';
-import { die, exitCodeForSignal, print, readJsonFile } from './util';
+import { watch } from 'chokidar';
+import { die, print, readJsonFile } from './util';
 import { CliContext } from './interfaces';
 
 export const minVersion = '4.0.0';
@@ -167,25 +168,8 @@ export default function install(context: CliContext) {
 			]);
 		})
 		.action(async (args, command) => {
-			const config = command.config || 'intern.json';
-
-			try {
-				statSync(config);
-			} catch (error) {
-				die([
-					`There isn't a test config at "${config}".` +
-						' You can specify a different test config ' +
-						'with the --config option, or run `intern init` ' +
-						'to setup a project for testing.'
-				]);
-			}
-
-			const internCmd = join(internDir, 'bin', 'intern.js');
-
-			// Allow user-specified args in the standard intern format to be passed through
+			const configFile = command.config || 'intern.json';
 			const internArgs = args || [];
-
-			internArgs.push(`config=${config}`);
 
 			for (const suite of command.suites) {
 				internArgs.push(`suites=${suite}`);
@@ -232,41 +216,60 @@ export default function install(context: CliContext) {
 				internArgs.push('verbose');
 			}
 
-			const nodeArgs: string[] = [];
-
-			if (command.debug) {
-				nodeArgs.push('debug');
-			}
-
-			const spawnArgs = [...nodeArgs, internCmd, ...internArgs];
-
-			vlog('Running %s %s', process.execPath, spawnArgs.join(' '));
-
-			const intern = spawn(process.execPath, spawnArgs, {
-				stdio: 'inherit'
-			});
-
-			process.on('SIGINT', () => intern.kill('SIGINT'));
-
-			intern.on('close', (code: number, signal: string) => {
-				if (process.exitCode == null) {
-					process.exitCode =
-						code != null ? code : exitCodeForSignal(signal);
-				}
-			});
-
-			intern.on('error', () => {
-				if (process.exitCode == null) {
-					process.exitCode = 1;
-				}
-			});
+			await runIntern(internDir, configFile, command.debug, internArgs);
 		});
+
+	commands.watch.action(async (_files, command) => {
+		const configFile = command.config || 'intern.json';
+		const { getConfig } = require(join(internDir, 'lib', 'node', 'util'));
+		const config = await getConfig(configFile);
+		const nodeSuites = [
+			...config.suites,
+			...(config.node ? config.node.suites : [])
+		];
+
+		const watcher = watch(nodeSuites)
+			.on('ready', () => {
+				print('Watching', nodeSuites);
+				watcher.on('add', scheduleInternRun);
+				watcher.on('change', scheduleInternRun);
+			})
+			.on('error', (error: Error) => {
+				print('Watcher error:', error);
+			});
+
+		process.on('SIGINT', () => watcher.close());
+
+		let timer: number;
+		let suites = new Set();
+		function scheduleInternRun(suite: string) {
+			suites.add(suite);
+			if (timer) {
+				clearTimeout(timer);
+			}
+			timer = setTimeout(async () => {
+				const toTest = Array.from(suites.values());
+				suites = new Set();
+
+				const args = [
+					'environments=',
+					...toTest.map(suite => `suites=${suite}`)
+				];
+				await runIntern(internDir, configFile, command.debug, args);
+			});
+		}
+
+		await runIntern(internDir, configFile, command.debug, [
+			'environments='
+		]);
+	});
 
 	commands.serve.action((args, command) => {
 		const config = command.config || 'intern.json';
 		const internCmd = join(internDir, 'bin', 'intern.js');
 
-		// Allow user-specified args in the standard intern format to be passed through
+		// Allow user-specified args in the standard intern format to be passed
+		// through
 		const internArgs = args || [];
 
 		internArgs.push(`config=${config}`);
@@ -334,4 +337,51 @@ export default function install(context: CliContext) {
 			intern.kill('SIGINT');
 		});
 	});
+
+	function runIntern(
+		internDir: string,
+		configFile: string,
+		debug: boolean,
+		args?: any[]
+	) {
+		try {
+			statSync(configFile);
+		} catch (error) {
+			die([
+				`There isn't a test config at "${configFile}".` +
+					' You can specify a different test config ' +
+					'with the --config option, or run `intern init` ' +
+					'to setup a project for testing.'
+			]);
+		}
+
+		const internArgs = args || [];
+		internArgs.push(`config=${configFile}`);
+
+		const internCmd = join(internDir, 'bin', 'intern.js');
+		const nodeArgs: string[] = debug ? ['debug'] : [];
+		const spawnArgs = [...nodeArgs, internCmd, ...internArgs];
+		let signalHandler: () => void;
+
+		vlog('Running %s %s', process.execPath, spawnArgs.join(' '));
+
+		return new Promise((resolve, reject) => {
+			const intern = spawn(process.execPath, spawnArgs, {
+				stdio: 'inherit'
+			});
+
+			signalHandler = () => intern.kill('SIGINT');
+
+			process.on('SIGINT', signalHandler);
+			intern.on('close', resolve);
+			intern.on('error', reject);
+		})
+			.then(() => {
+				process.removeListener('SIGINT', signalHandler);
+			})
+			.catch(error => {
+				process.removeListener('SIGINT', signalHandler);
+				throw error;
+			});
+	}
 }
