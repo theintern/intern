024e6170e2d509a8a2bd12db0e8c52d24b3ffd5d
Jason Cheatham <j.cheatham@gmail.com>
2017-09-11 13:38:46 -0400

    Install intern 4 in project if necessary

--- src/bin/intern.ts
+++ src/bin/intern.ts
@@ -4,6 +4,8 @@ import * as program from 'commander';
 import { readFileSync } from 'fs';
 import { dirname, join } from 'path';
 import { sync as resolve } from 'resolve';
+import { createInterface } from 'readline';
+import { execSync } from 'child_process';
 
 import {
 	acceptVersion,
@@ -25,20 +27,6 @@ import cli4, {
 
 let internDir: any;
 let internPkg: any;
-
-try {
-	internDir = dirname(resolve('intern', { basedir: process.cwd() }));
-	internPkg = JSON.parse(
-		readFileSync(join(internDir, 'package.json'), { encoding: 'utf8' })
-	);
-} catch (error) {
-	die([
-		'You will need a local install of Intern before you can use this ' +
-			'command. Install it with',
-		'',
-		'  npm install --save-dev intern'
-	]);
-}
 
 const pkg = require('../../../package.json');
 const minVersion = '3.0.0';
@@ -228,30 +216,84 @@ commands['*'] = program
 		program.outputHelp();
 	});
 
-const context = {
-	browsers,
-	commands,
-	program,
-	vlog,
-	internDir,
-	internPkg,
-	testsDir
-};
+(async () => {
+	try {
+		internDir = dirname(resolve('intern', { basedir: process.cwd() }));
+		internPkg = JSON.parse(
+			readFileSync(join(internDir, 'package.json'), { encoding: 'utf8' })
+		);
+	} catch (error) {
+		const rl = createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
 
-if (acceptVersion(internPkg.version, cli3Min, cli3Max)) {
-	cli3(context);
-} else if (acceptVersion(internPkg.version, cli4Min, cli4Max)) {
-	cli4(context);
-} else {
-	die(
-		`This command requires Intern ${minVersion} or newer (` +
-			`${internPkg.version} is installed).`
-	);
-}
+		try {
+			print([
+				'',
+				'You need a local install of Intern to use this command.',
+				''
+			]);
+			const shouldInstall = await new Promise(resolve => {
+				rl.question('  Install intern@latest now? ', answer => {
+					resolve(answer.toLowerCase()[0] === 'y');
+				});
+			});
 
-program.parse(process.argv);
+			if (shouldInstall) {
+				print();
+				execSync('npm install intern@^4.0.0-alpha --save-dev', {
+					stdio: 'inherit'
+				});
+			}
+		} finally {
+			rl.close();
+		}
+	}
 
-// If no command was given, show the help message
-if (process.argv.length === 2) {
-	program.outputHelp();
-}
+	if (!internPkg) {
+		try {
+			internDir = dirname(resolve('intern', { basedir: process.cwd() }));
+			internPkg = JSON.parse(
+				readFileSync(join(internDir, 'package.json'), {
+					encoding: 'utf8'
+				})
+			);
+		} catch (error) {
+			die([
+				'You can install the latest Intern with:',
+				'',
+				'  npm install --save-dev intern@^4.0.0-alpha',
+				''
+			]);
+		}
+	}
+
+	const context = {
+		browsers,
+		commands,
+		program,
+		vlog,
+		internDir,
+		internPkg,
+		testsDir
+	};
+
+	if (acceptVersion(internPkg.version, cli3Min, cli3Max)) {
+		cli3(context);
+	} else if (acceptVersion(internPkg.version, cli4Min, cli4Max)) {
+		cli4(context);
+	} else {
+		die(
+			`This command requires Intern ${minVersion} or newer (` +
+				`${internPkg.version} is installed).`
+		);
+	}
+
+	program.parse(process.argv);
+
+	// If no command was given, show the help message
+	if (process.argv.length === 2) {
+		program.outputHelp();
+	}
+})();
