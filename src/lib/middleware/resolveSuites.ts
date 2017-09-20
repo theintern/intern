e30f0868629ed782cc28fcc455e6eb17fc88d6f4
Jason Cheatham <j.cheatham@gmail.com>
2017-09-20 09:51:06 -0400

    Remove a debugging statement

--- src/lib/middleware/resolveSuites.ts
+++ src/lib/middleware/resolveSuites.ts
@@ -4,7 +4,6 @@ import { expandFiles } from '../node/util';
 
 export default function instrument(): InternRequestHandler {
 	return (request, response) => {
-		console.log('calling resolver');
 		const query = parse(request.url!, true).query;
 		const suites = Array.isArray(query.suites)
 			? query.suites
