2e4b83294a92dae1154f4cfe90f747c9a8c2acf6
Jason Cheatham <j.cheatham@gmail.com>
2018-03-13 21:02:05 -0400

    Improve decompress types

---
+++ types/decompress/index.d.ts
@@ -0,0 +1,33 @@
+declare module 'decompress' {
+	function decompress(
+		input: string | Buffer,
+		output?: string,
+		options?: decompress.Options
+	): Promise<decompress.ParsedFile>;
+
+	namespace decompress {
+		interface FileDescriptor {
+			mode: number;
+			mtime: string;
+			path: string;
+			type: string;
+		}
+
+		interface ParsedFile extends FileDescriptor {
+			data: Buffer;
+		}
+
+		interface Plugin {
+			(): (buf: Buffer) => Promise<Buffer>;
+		}
+
+		export interface Options {
+			filter?(file: FileDescriptor): boolean;
+			map?(file: FileDescriptor): FileDescriptor;
+			plugins?: Plugin[];
+			strip?: number;
+		}
+	}
+
+	export = decompress;
+}
