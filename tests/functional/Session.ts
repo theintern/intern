998bba9046997605893d87ec05cddf78d69879ae
Jason Cheatham <j.cheatham@gmail.com>
2019-06-13 08:48:06 -0400

    Update timeout for screenshot test
    It's frequently timing out for IE11

--- tests/functional/Session.ts
+++ tests/functional/Session.ts
@@ -610,7 +610,7 @@ registerSuite('Session', () => {
 
         const magic = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
 
-        this.async(30000);
+        this.async(60000);
 
         return session.takeScreenshot().then(function(screenshot: any) {
           /*jshint node:true */
