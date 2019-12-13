d07918905e98f8bfe5ef1bf2836638f10ef7fbb9
Jason Cheatham <j.cheatham@gmail.com>
2019-12-13 14:21:07 -0500

    Argument handling updates for cli
    - Handle args with spaces
    - Change --no-instrument to --no-coverage

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,15 +1,13 @@
-#!/usr/bin/env node
-
 import { exec, log } from './lib/util';
+import { quote } from 'shell-quote';
 
-const args = process.argv.slice(2).join(' ');
+const args = quote(process.argv.slice(2));
 let command = 'ts-node -r tsconfig-paths/register src/bin/intern.ts run';
 if (args) {
   command += ` ${args}`;
 }
 
-log('Running tests');
-log(command);
+log('Running tests...');
 try {
   exec(command, { silent: false });
 } catch (error) {
