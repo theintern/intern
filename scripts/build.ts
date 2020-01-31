568258ede404350974fa05181e6afd73883d6cc9
Jason Cheatham <j.cheatham@gmail.com>
2020-01-31 10:47:00 -0500

    Improve handling of build errors
    If a build task fails, ensure the build script exits with a non-zero
    exit code.
    
    fixes #1085

--- scripts/build.ts
+++ scripts/build.ts
@@ -11,42 +11,59 @@ import exec from './lib/exec';
 import { baseDir, copy, copyAll, log, logError } from './lib/util';
 import { watchFiles, watchProcess } from './lib/watch';
 
+function copyFiles(buildDir: string) {
+  copyAll([{ base: 'src', pattern: '**/*.{d.ts,js.png}' }], buildDir);
+  copy('schemas', buildDir);
+  copy('README.md', buildDir);
+  copy('LICENSE', buildDir);
+  copy('index.html', buildDir, 'src');
+  copy('favicon.png', buildDir, 'src');
+  copy(
+    'remote.html',
+    join(buildDir, 'browser'),
+    join('src', 'core', 'browser')
+  );
+
+  const pkgJson = JSON.parse(
+    readFileSync(join(baseDir, 'package.json'), { encoding: 'utf8' })
+  );
+  delete pkgJson['lint-staged'];
+  delete pkgJson['pre-commit'];
+  delete pkgJson.prettier;
+  delete pkgJson.devDependencies;
+  writeFileSync(
+    `${buildDir}/package.json`,
+    JSON.stringify(pkgJson, null, '  ')
+  );
+}
+
 (async () => {
-  function handleError(error: Error) {
-    if (error.name === 'ExecError') {
-      logError((<any>error).stderr || (<any>error).stdout);
-      process.exit((<any>error).code);
-    } else {
-      throw error;
-    }
-  }
+  try {
+    let watchMode = false;
+    let outDirOpt: string | undefined;
+    let pack = false;
 
-  let watchMode = false;
-  let outDirOpt: string | undefined;
-  let pack = false;
-
-  const args = process.argv.slice(2);
-  while (args.length > 0) {
-    const arg = args.shift();
-    if (arg === '-w' || arg == '--watch') {
-      watchMode = true;
-    } else if (arg == '--outdir' || arg == '-o') {
-      outDirOpt = args.shift();
-      if (!outDirOpt) {
-        throw new Error('-o / --outdir needs an output directory');
+    const args = process.argv.slice(2);
+    while (args.length > 0) {
+      const arg = args.shift();
+      if (arg === '-w' || arg == '--watch') {
+        watchMode = true;
+      } else if (arg == '--outdir' || arg == '-o') {
+        outDirOpt = args.shift();
+        if (!outDirOpt) {
+          throw new Error('-o / --outdir needs an output directory');
+        }
+      } else if (arg === '--pack' || arg === '-p') {
+        pack = true;
       }
-    } else if (arg === '--pack' || arg === '-p') {
-      pack = true;
     }
-  }
 
-  const outDir = outDirOpt;
+    const outDir = outDirOpt;
 
-  // -----------------------------------------------------------------
-  // Typescript
-  // -----------------------------------------------------------------
-  for (const suffix of ['bin', 'lib']) {
-    try {
+    // -----------------------------------------------------------------
+    // Typescript
+    // -----------------------------------------------------------------
+    for (const suffix of ['bin', 'lib']) {
       const tsconfig = `${baseDir}/tsconfig-${suffix}.json`;
       const cmd = ['npx', 'tsc', '-p', tsconfig];
 
@@ -61,15 +78,11 @@ import { watchFiles, watchProcess } from './lib/watch';
       } else {
         await exec(cmd[0], cmd.slice(1));
       }
-    } catch (error) {
-      handleError(error);
     }
-  }
 
-  // -----------------------------------------------------------------
-  // Webpack
-  // -----------------------------------------------------------------
-  try {
+    // -----------------------------------------------------------------
+    // Webpack
+    // -----------------------------------------------------------------
     log('Running webpack...');
     const cmd = ['npx', 'webpack'];
 
@@ -84,64 +97,39 @@ import { watchFiles, watchProcess } from './lib/watch';
     } else {
       await exec(cmd[0], cmd.slice(1));
     }
+
+    // -----------------------------------------------------------------
+    // Resources
+    // -----------------------------------------------------------------
+    log('Copying resources...');
+    const buildDir = join(baseDir, outDir || '_build');
+
+    copyFiles(buildDir);
+
+    if (watchMode) {
+      watchFiles(
+        [
+          'src/**/*.{styl,d.ts,html,js.png}',
+          'schemas/**',
+          'README.md',
+          'LICENSE',
+          'package.json'
+        ],
+        () => copyFiles(buildDir)
+      );
+    }
+
+    // -----------------------------------------------------------------
+    // Packaging
+    // -----------------------------------------------------------------
+    if (pack) {
+      log('Creating package tarball...');
+      await exec('npm', ['pack', buildDir]);
+    }
+
+    log('Done building Intern');
   } catch (error) {
-    handleError(error);
+    logError(error);
+    process.exitCode = 1;
   }
-
-  // -----------------------------------------------------------------
-  // Resources
-  // -----------------------------------------------------------------
-  log('Copying resources...');
-  const buildDir = join(baseDir, outDir || '_build');
-
-  function copyFiles() {
-    copyAll([{ base: 'src', pattern: '**/*.{d.ts,js.png}' }], buildDir);
-    copy('schemas', buildDir);
-    copy('README.md', buildDir);
-    copy('LICENSE', buildDir);
-    copy('index.html', buildDir, 'src');
-    copy('favicon.png', buildDir, 'src');
-    copy(
-      'remote.html',
-      join(buildDir, 'browser'),
-      join('src', 'core', 'browser')
-    );
-
-    const pkgJson = JSON.parse(
-      readFileSync(join(baseDir, 'package.json'), { encoding: 'utf8' })
-    );
-    delete pkgJson['lint-staged'];
-    delete pkgJson['pre-commit'];
-    delete pkgJson.prettier;
-    delete pkgJson.devDependencies;
-    writeFileSync(
-      `${buildDir}/package.json`,
-      JSON.stringify(pkgJson, null, '  ')
-    );
-  }
-
-  copyFiles();
-
-  if (watchMode) {
-    watchFiles(
-      [
-        'src/**/*.{styl,d.ts,html,js.png}',
-        'schemas/**',
-        'README.md',
-        'LICENSE',
-        'package.json'
-      ],
-      copyFiles
-    );
-  }
-
-  // -----------------------------------------------------------------
-  // Packaging
-  // -----------------------------------------------------------------
-  if (pack) {
-    log('Creating package tarball...');
-    await exec('npm', ['pack', buildDir]);
-  }
-
-  log('Done building Intern');
 })();
