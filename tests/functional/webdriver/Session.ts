294849334395c0670f3c9725a4b73935a5eb0792
Jason Cheatham <j.cheatham@gmail.com>
2020-02-26 13:55:52 -0500

    test: loosen check on Session timeout test
    references #1060

--- tests/functional/webdriver/Session.ts
+++ tests/functional/webdriver/Session.ts
@@ -1146,10 +1146,10 @@ registerSuite('functional/webdriver/Session', () => {
                   );
                 },
                 function() {
-                  assert.operator(
+                  assert.closeTo(
                     Date.now() - startTime,
-                    '>=',
                     2000,
+                    50,
                     'Driver should wait for implicit timeout before continuing'
                   );
                 }
