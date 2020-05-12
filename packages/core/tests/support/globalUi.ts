26bc3b35f9b8238b433962fce682dd8084baa81e
Jason Cheatham <jason@jasoncheatham.com>
2020-05-12 15:51:31 -0400

    test(core): add chai-exclude plugin
    Adds deepEqualExclude assertion

--- packages/core/tests/support/globalUi.ts
+++ packages/core/tests/support/globalUi.ts
@@ -4,8 +4,10 @@
  * Install some commonly used test functionals globally
  */
 import { global } from '@theintern/common';
+import chaiExclude from 'chai-exclude';
 
 const chai = intern.getPlugin('chai');
+chai.use(chaiExclude);
 global.assert = chai.assert;
 
 const { registerSuite } = intern.getPlugin('interface.object');
