1f8c83898ce650c46764d7c84aef939edecc1bab
Jason Cheatham <jason@jasoncheatham.com>
2020-05-13 16:54:17 -0400

    test(digdug): run Sauce relay on non-standard port
    During integration tests, Sauce's Selenium relay should run on a
    different port than the active test tunnel.

--- packages/digdug/tests/integration/SauceLabsTunnel.ts
+++ packages/digdug/tests/integration/SauceLabsTunnel.ts
@@ -10,7 +10,10 @@ function checkEnvironment(environment: any) {
 let suite = {};
 suite = addEnvironmentTest(suite, SauceLabsTunnel, checkEnvironment);
 suite = addStartStopTest(suite, SauceLabsTunnel, {
-  timeout: 120000
+  timeout: 120000,
+  // Use a non-standard port to not conflict with a running tunnel in the
+  // host Intern
+  port: 39823
 });
 
 registerSuite('integration/SauceLabsTunnel', suite);
